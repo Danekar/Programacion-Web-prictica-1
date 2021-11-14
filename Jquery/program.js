7bb49b9fda89ab1418409fa8f5765c595a03aafc
Danekar <danilaesp001@users.noreply.github.com>
2021-11-14 15:42:27 +0100

    jqerizado

--- Jquery/program.js
+++ Jquery/program.js
@@ -1,8 +1,10 @@
+var jugadorPun=0;
+var maquinaPun=0;
+
 $(document).ready(function(){
     llaveEscondedor();
     $("img").click(function(){
-      $(this).attr("src", "../imagenes/mano-con-llave.png");
-      
+      llaveBuscador(this);
     });
   });
 
@@ -12,9 +14,41 @@ $(document).ready(function(){
       var llave = Math.floor(Math.random() * 2);
 
       if(llave==0){
-        document.getElementById("imgDerecha").alt="llave";
+        $("#derecha").attr("alt", "llave");
       }else{
-        document.getElementById("imgIzquierda").alt="llave";
+        $("#izquierda").attr("alt", "llave");
+    }
+  }
+
+  function llaveBuscador(mano){
+    if(mano.alt=="llave"){
+        $(mano).attr("src", "../imagenes/mano-con-llave.png");
+        jugadorPun++;
+        $("#jugador").html("Puntos del jugador: "+jugadorPun);
+    }else{
+        maquinaPun++;
+        $("#maquina").html("Puntos de la maquina: "+maquinaPun);
+       
+    }
+  }
+
+  function reinicia(){
+    jugadorPun=0;
+    maquinaPun=0;
+    $("#jugador").html("Puntos del jugador: "+jugadorPun);
+    $("#maquina").html("Puntos de la maquina: "+maquinaPun);
+       
+  }
+
+  function pista(){
+    var manoDerecha = $("#derecha").attr("alt");
+    var manoIzquierda = $("#izquierda").attr("alt");
+
+    if(manoDerecha=="llave"){
+        $("#pista").html("La llave está en la mano: derecha");  
+    }else{
+        $("#pista").html("La llave está en la mano: izquierda");  
+
     }
   }
 
