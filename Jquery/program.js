e12666651927d21a22ab6665a7a2c66c8554a370
Danekar <danilaesp001@users.noreply.github.com>
2021-11-14 14:00:57 +0100

    pococ a poco el java script

--- Jquery/program.js
+++ Jquery/program.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
+    llaveEscondedor();
     $("img").click(function(){
       $(this).attr("src", "../imagenes/mano-con-llave.png");
+      
     });
   });
+
+  window.onload = llaveEscondedor()
+
+  function llaveEscondedor(){
+      var llave = Math.floor(Math.random() * 2);
+
+      if(llave==0){
+        document.getElementById("imgDerecha").alt="llave";
+      }else{
+        document.getElementById("imgIzquierda").alt="llave";
+    }
+  }
+
